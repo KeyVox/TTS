fe2c674a3c96adceffd8e3525de2bdadfa47aff7
luisrodolfo <www.luisrodolfo@hotmail.com>
2020-10-18 09:22:46 -0500

    Create tbl_audio.sql

---
+++ database/bd_ivr/tbl_audio.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `tbl_audio` (
+  `id` int(11) DEFAULT NULL,
+  `aleatorio` int(11) DEFAULT NULL,
+  `voz` varchar(100) DEFAULT NULL,
+  `mensaje` varchar(5000) DEFAULT NULL,
+  `generado` int(11) DEFAULT NULL,
+  `cpuname` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
